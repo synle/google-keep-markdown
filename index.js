cc0a02f9ebfe953ca94fd8de4b82ea7c0311acbb
Sy Le <lenguyensy@gmail.com>
2021-04-14 10:48:57 -0700

    Code format

--- index.js
+++ index.js
@@ -8,16 +8,16 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+(function () {
+  "use strict";
 
-    let target = document.body;
+  let target = document.body;
   const isDarkMode = new Date().getHours() >= 19 || new Date().getHours() <= 8;
   let colorIdx = 0;
-  let styleDom = document.getElementById('style-dialog');
-  if(!styleDom){
+  let styleDom = document.getElementById("style-dialog");
+  if (!styleDom) {
     styleDom = document.createElement("style");
-    styleDom.id= 'style-dialog'
+    styleDom.id = "style-dialog";
     styleDom.innerText = `
       :root{
         --color-bg: #282a36;
@@ -121,139 +121,159 @@
           grid-template-columns: 1fr 900px;
         }
       }
-    `
+    `;
     target.appendChild(styleDom);
 
     let newScript = document.createElement("script");
     newScript.src = "https://cdn.jsdelivr.net/npm/marked/marked.min.js";
     target.appendChild(newScript);
 
-    window.addEventListener('keydown', (e) =>{
+    window.addEventListener("keydown", (e) => {
       let stop = false;
 
       const keycode = parseInt(e.keyCode);
 
-      if(keycode === 9 && isTargetContentDom(e.target))  { // TAB to switch color
+      if (keycode === 9 && isTargetContentDom(e.target)) {
+        // TAB to switch color
         switchColor();
         stop = true;
       }
 
-      if(stop){
+      if (stop) {
         e.preventDefault();
         e.stopPropagation();
         contentDom.focus();
         return false;
       }
-    })
+    });
   }
 
-  let contentDom = document.getElementById('content-dialog');
+  let contentDom = document.getElementById("content-dialog");
 
-  async function _formatMarkdownForKeep(suppressError){
-    const origTitleDom =document.querySelectorAll(`[contenteditable="true"][spellcheck="true"]`)[0]
-    const origContentDom =document.querySelectorAll(`[contenteditable="true"][spellcheck="true"]`)[1]
-    contentDom = document.getElementById('content-dialog');
+  async function _formatMarkdownForKeep(suppressError) {
+    const origTitleDom = document.querySelectorAll(
+      `[contenteditable="true"][spellcheck="true"]`
+    )[0];
+    const origContentDom = document.querySelectorAll(
+      `[contenteditable="true"][spellcheck="true"]`
+    )[1];
+    contentDom = document.getElementById("content-dialog");
 
     let shouldBindData = false;
-    try{
-      shouldBindData = origContentDom && origContentDom.innerText.trim().length > 0;
-    }catch(err){}
-
+    try {
+      shouldBindData =
+        origContentDom && origContentDom.innerText.trim().length > 0;
+    } catch (err) {}
 
     let width = 600;
-    if(shouldBindData){
+    if (shouldBindData) {
       width = origContentDom.offsetWidth + 40;
     }
 
-    if(!contentDom){
+    if (!contentDom) {
       contentDom = document.createElement("div");
-      contentDom.id = 'content-dialog';
-      contentDom.contentEditable = true
+      contentDom.id = "content-dialog";
+      contentDom.contentEditable = true;
       //contentDom.addEventListener('blur', () => contentDom.remove())
-      contentDom.addEventListener('keydown', (e) =>{
+      contentDom.addEventListener("keydown", (e) => {
         let stop = false;
 
         const keycode = parseInt(e.keyCode);
-        if(keycode === 27){
+        if (keycode === 27) {
           // escape key
           contentDom.remove();
           stop = true;
-
         }
 
-        if(stop){
+        if (stop) {
           e.preventDefault();
           e.stopPropagation();
 
           origContentDom.focus();
           return false;
         }
-      })
+      });
       target.appendChild(contentDom);
     }
 
-    if(shouldBindData){
+    if (shouldBindData) {
       // clean the dom
-      contentDom.innerText = '';
+      contentDom.innerText = "";
 
-      const articleContent = document.createElement('article');
-      contentDom.append(articleContent)
+      const articleContent = document.createElement("article");
+      contentDom.append(articleContent);
 
       // force focus on the content dom to stop google keep from
       // doing the infinite scroll loader
-      contentDom.addEventListener('blur', () => {
+      contentDom.addEventListener("blur", () => {
         contentDom.focus();
-      })
+      });
 
-      let newContentHtml = origContentDom.innerText.includes('```')
-      if(document.querySelectorAll(`[contenteditable="true"][aria-label="list item"]`).length > 0) {
-          // is a list of checkboxes
-          newContentHtml = [...document.querySelectorAll(`[contenteditable="true"][aria-label="list item"]`)].map(d => `<p>${d.innerText.trim()}</p>`).join('')
-      } else if(origContentDom.innerText.includes('```')){
-          // is markdown content
-          newContentHtml = marked(origContentDom.innerText).trim()
+      let newContentHtml = origContentDom.innerText.includes("```");
+      if (
+        document.querySelectorAll(
+          `[contenteditable="true"][aria-label="list item"]`
+        ).length > 0
+      ) {
+        // is a list of checkboxes
+        newContentHtml = [
+          ...document.querySelectorAll(
+            `[contenteditable="true"][aria-label="list item"]`
+          ),
+        ]
+          .map((d) => `<p>${d.innerText.trim()}</p>`)
+          .join("");
+      } else if (origContentDom.innerText.includes("```")) {
+        // is markdown content
+        newContentHtml = marked(origContentDom.innerText).trim();
       } else {
-          // is a plain text
-          newContentHtml = origContentDom.innerText.trim().replace(/\n/g, '<br />')
+        // is a plain text
+        newContentHtml = origContentDom.innerText
+          .trim()
+          .replace(/\n/g, "<br />");
       }
-          
-          
 
       // append the content
-      newContentHtml = `<h2>${origTitleDom.innerText}</h2><hr />` + newContentHtml
+      newContentHtml =
+        `<h2>${origTitleDom.innerText}</h2><hr />` + newContentHtml;
 
-      if (newContentHtml !== articleContent.innerHTML && document.activeElement.id !== 'content-dialog') {
-          articleContent.innerHTML = newContentHtml;
+      if (
+        newContentHtml !== articleContent.innerHTML &&
+        document.activeElement.id !== "content-dialog"
+      ) {
+        articleContent.innerHTML = newContentHtml;
 
-          // append images
-          const noteImages = origTitleDom.previousSibling.querySelectorAll('img');
-          if (noteImages.length > 0) {
-              const figureImages = document.createElement('figure');
-              contentDom.append(figureImages)
+        // append images
+        const noteImages = origTitleDom.previousSibling.querySelectorAll("img");
+        if (noteImages.length > 0) {
+          const figureImages = document.createElement("figure");
+          contentDom.append(figureImages);
 
-              const requestPromises = [];
-              for (const img of origTitleDom.previousSibling.querySelectorAll('img')) {
-                  const newImg = document.createElement("div");
-                  // newImg.innerHTML = `Loading Image...`;
-                  requestPromises.push(parseAndInsertImageAsBase64(img.src, newImg));
-                  figureImages.append(newImg)
-              }
-
-              await Promise.allSettled(requestPromises)
+          const requestPromises = [];
+          for (const img of origTitleDom.previousSibling.querySelectorAll(
+            "img"
+          )) {
+            const newImg = document.createElement("div");
+            // newImg.innerHTML = `Loading Image...`;
+            requestPromises.push(parseAndInsertImageAsBase64(img.src, newImg));
+            figureImages.append(newImg);
           }
+
+          await Promise.allSettled(requestPromises);
+        }
       }
 
       // add the close button
-      const closeContentBtn = document.createElement('button');
-      closeContentBtn.innerText = 'X'
-      closeContentBtn.id = 'content-dialog-close-btn'
-      closeContentBtn.addEventListener('click', () => {
+      const closeContentBtn = document.createElement("button");
+      closeContentBtn.innerText = "X";
+      closeContentBtn.id = "content-dialog-close-btn";
+      closeContentBtn.addEventListener("click", () => {
         contentDom.remove();
-      })
-      articleContent.append(closeContentBtn)
+      });
+      articleContent.append(closeContentBtn);
 
-      for(const anchor of contentDom.querySelectorAll('a')){
-        anchor.target = '_blank'
+      for (const anchor of contentDom.querySelectorAll("a")) {
+        anchor.target = "_blank";
         anchor.contentEditable = false;
       }
 
@@ -261,63 +281,63 @@
     } else {
       // setTimeout(_formatMarkdownForKeep, 300);
       contentDom.remove();
-      if(suppressError !== true){
-        alert('Preview is not supported');
+      if (suppressError !== true) {
+        alert("Preview is not supported");
       }
     }
   }
 
-  function switchColor(){
+  function switchColor() {
     // tab key or shift
-    var r = document.querySelector(':root');
+    var r = document.querySelector(":root");
     colorIdx++;
-    if(colorIdx % 2 === 0){
+    if (colorIdx % 2 === 0) {
       // dark
-      r.style.setProperty('--color-bg', '#282a36');
-      r.style.setProperty('--color-fg', '#f8f8f2');
-      r.style.setProperty('--color-link', '#50fa7b');
-      r.style.setProperty('--color-border', '#f1fa8c');
+      r.style.setProperty("--color-bg", "#282a36");
+      r.style.setProperty("--color-fg", "#f8f8f2");
+      r.style.setProperty("--color-link", "#50fa7b");
+      r.style.setProperty("--color-border", "#f1fa8c");
     } else {
       // light
-      r.style.setProperty('--color-bg', '#f8f8f2');
-      r.style.setProperty('--color-fg', '#282a36');
-      r.style.setProperty('--color-link', '#ff5555');
-      r.style.setProperty('--color-border', '#44475a');
+      r.style.setProperty("--color-bg", "#f8f8f2");
+      r.style.setProperty("--color-fg", "#282a36");
+      r.style.setProperty("--color-link", "#ff5555");
+      r.style.setProperty("--color-border", "#44475a");
     }
   }
 
   function toDataURL(url, callback) {
     var xhr = new XMLHttpRequest();
-    xhr.onload = function() {
+    xhr.onload = function () {
       var reader = new FileReader();
-      reader.onloadend = function() {
+      reader.onloadend = function () {
         callback(reader.result);
-      }
+      };
       reader.readAsDataURL(xhr.response);
     };
-    xhr.open('GET', url);
-    xhr.responseType = 'blob';
+    xhr.open("GET", url);
+    xhr.responseType = "blob";
     xhr.send();
   }
 
-  function parseAndInsertImageAsBase64(url, newImg){
-    return new Promise(resolve => {
-      toDataURL(url, function(dataUrl) {
+  function parseAndInsertImageAsBase64(url, newImg) {
+    return new Promise((resolve) => {
+      toDataURL(url, function (dataUrl) {
         newImg.innerHTML = `<a href='${url}'><img src="${dataUrl}" altText="Doc Image" /></a>`;
         resolve();
-      })
-    })
+      });
+    });
   }
 
   function isTargetContentDom(startDom) {
-      let targetDom = startDom
-      while (targetDom) {
-          if (targetDom && targetDom.id === 'content-dialog') {
-              return true;
-          }
-          targetDom = targetDom.parentElement;
+    let targetDom = startDom;
+    while (targetDom) {
+      if (targetDom && targetDom.id === "content-dialog") {
+        return true;
       }
-      return false;
+      targetDom = targetDom.parentElement;
+    }
+    return false;
   }
 
   switchColor();
@@ -329,35 +349,40 @@
     // listen on dom mutation
     // Callback function to execute when mutations are observed
     const _setupFormatButtonCallback = () => {
-      let noteCloseBtns = []
-      for(let button of document.querySelectorAll('[role="button"')){
-        if(button.innerText.trim() === 'Close'){
-          noteCloseBtns.push(button)
+      let noteCloseBtns = [];
+      for (let button of document.querySelectorAll('[role="button"')) {
+        if (button.innerText.trim() === "Close") {
+          noteCloseBtns.push(button);
         }
       }
 
       // insert this button
-      for(let noteCloseBtn of noteCloseBtns){
-        if(noteCloseBtn && noteCloseBtn.parentElement.querySelectorAll('.format-markdown-modal-btn').length === 0){
-          const formatButton = document.createElement('div');
-          formatButton.innerText = 'Format'
-          formatButton.className = `format-markdown-modal-btn ${noteCloseBtn.className}`
+      for (let noteCloseBtn of noteCloseBtns) {
+        if (
+          noteCloseBtn &&
+          noteCloseBtn.parentElement.querySelectorAll(
+            ".format-markdown-modal-btn"
+          ).length === 0
+        ) {
+          const formatButton = document.createElement("div");
+          formatButton.innerText = "Format";
+          formatButton.className = `format-markdown-modal-btn ${noteCloseBtn.className}`;
           formatButton.style.marginRight = 0;
-          formatButton.style.background = 'red';
-          formatButton.style.color = 'white';
-          formatButton.addEventListener('click', _formatMarkdownForKeep)
+          formatButton.style.background = "red";
+          formatButton.style.color = "white";
+          formatButton.addEventListener("click", _formatMarkdownForKeep);
           noteCloseBtn.parentElement.prepend(formatButton);
         }
       }
-    }
+    };
 
     // Create an observer instance linked to the callback function
     // Start observing the target node for configured mutations
-    const targetNode = document.querySelector('.notes-container');
+    const targetNode = document.querySelector(".notes-container");
     new MutationObserver(_setupFormatButtonCallback).observe(targetNode, {
       attributes: true,
       childList: true,
-      subtree: false
+      subtree: false,
     });
   }, 500);
 })();
